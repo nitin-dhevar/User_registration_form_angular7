72787d5f9089c673f2bbaa446a6db4026d6672e3
nitin-dhevar <46418326+nitin-dhevar@users.noreply.github.com>
2020-07-08 12:17:31 +0530

    Home page user details added...

---
+++ src/app/_models/user.ts
@@ -0,0 +1,10 @@
+﻿export class User {
+    id: number;
+    email: string;
+    password: string;
+    country: string;
+    state: string;
+    firstName: string;
+    gender: string;
+    token: string;
+}
